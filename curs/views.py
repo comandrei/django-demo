699f8ad64d96e7d66eec397cadbf550b6180b944
Andrei Coman <comandrei@gmail.com>
2021-05-22 12:26:56 +0300

    Send data to template

--- curs/views.py
+++ curs/views.py
@@ -6,4 +6,5 @@ def hello(request):
     return render(request, "index.html", {'name': 'dorel'})
 
 def show_students(request):
-    return render(request, "show_students.html")
+    lista_studenti = ["student 1", "student 2", "student 3"]
+    return render(request, "show_students.html", {"students": lista_studenti})
