1062e4f13090f2f6849d0a79422f2568a946d0dc
Andrei Coman <comandrei@gmail.com>
2021-05-22 13:17:53 +0300

    Add default ordering

--- curs/models.py
+++ curs/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    class Meta:
+        ordering = ("nume", )
     nume = models.CharField(max_length=20)
     prenume = models.CharField(max_length=20)
 
