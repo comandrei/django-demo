3ce6026bcae75599f8166077580aa50e79fc8bad
Andrei Coman <comandrei@gmail.com>
2021-05-22 12:07:46 +0300

    Change field type on Student

--- curs/models.py
+++ curs/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    nume = models.TextField()
-    prenume = models.TextField()
+    nume = models.CharField(max_length=20)
+    prenume = models.CharField(max_length=20)
