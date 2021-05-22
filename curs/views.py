5173373a13b2bb3710242cba4c1f15d40d52d510
Andrei Coman <comandrei@gmail.com>
2021-05-22 12:44:22 +0300

    Filter users using GET parameters

--- curs/views.py
+++ curs/views.py
@@ -7,5 +7,9 @@ def hello(request):
     return render(request, "index.html", {'name': 'dorel'})
 
 def show_students(request):
-    lista_studenti = Student.objects.all()
+    if 'search' in request.GET:
+        search = request.GET['search']
+        lista_studenti = Student.objects.filter(nume=search)
+    else:
+        lista_studenti = Student.objects.all()
     return render(request, "show_students.html", {"students": lista_studenti})
