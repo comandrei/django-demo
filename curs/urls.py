a2f5e7df847d2a83ba6d427cdc791986e3718173
Andrei Coman <comandrei@gmail.com>
2021-05-22 13:48:15 +0300

    Switch to reverse based urls

--- curs/urls.py
+++ curs/urls.py
@@ -3,6 +3,6 @@ from .views import hello, show_students, show_student
 
 urlpatterns = [
     path('', hello),
-    path('students/', show_students),
-    path('student/<int:student_id>/', show_student)
+    path('students/', show_students, name="student_list"),
+    path('student/<int:student_id>/', show_student, name="student_detail")
 ] 
