ebd9d70bd4bf0d072d49b70b713ca5cd2e08a6ea
Andrei Coman <comandrei@gmail.com>
2021-05-22 13:17:06 +0300

    Filter by both columns

--- curs/views.py
+++ curs/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.db.models import Q
 from .models import Student
 
 def hello(request):
@@ -9,7 +9,8 @@ def hello(request):
 def show_students(request):
     if 'search' in request.GET:
         search = request.GET['search']
-        lista_studenti = Student.objects.filter(nume=search)
+        lista_studenti = Student.objects.filter(Q(nume__iexact=search) | Q(prenume__iexact=search))
+        print(str(lista_studenti.query))
     else:
         lista_studenti = Student.objects.all()
     return render(request, "show_students.html", {"students": lista_studenti})
