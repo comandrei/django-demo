9254ce9711490b81a32f173a48d4098667c36393
Andrei Coman <comandrei@gmail.com>
2021-05-29 10:44:08 +0300

    Add Curs

--- curs/models.py
+++ curs/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Curs(models.Model):
+    nume = models.CharField(max_length=20)
+
 class Student(models.Model):
     class Meta:
         ordering = ("nume", )
     nume = models.CharField(max_length=20)
     prenume = models.CharField(max_length=20, db_index=True)
-
+    cursuri = models.ManyToManyField(Curs)
+    
     def __str__(self):
         return f'{self.prenume} {self.nume}'
+
+
 
 class Note(models.Model):
     nota = models.IntegerField()
