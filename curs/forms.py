d489b0d23617f74f9d46ef40f4bae932a7d7b7ce
Andrei Coman <comandrei@gmail.com>
2021-06-05 12:45:46 +0300

    Override widget

--- curs/forms.py
+++ curs/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     nume = forms.CharField(max_length=20)
     email = forms.EmailField()
-    message = forms.CharField(max_length=100)
+    message = forms.CharField(max_length=100, widget=forms.Textarea)
