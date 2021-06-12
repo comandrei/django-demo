c1ecc9b27d75139dfe93fe6c3fb9900309345a35
Andrei Coman <comandrei@gmail.com>
2021-06-12 14:03:48 +0300

    Admin customization

--- curs/admin.py
+++ curs/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 from .models import Student, Note, Curs
 
-admin.site.register(Student)
 admin.site.register(Note)
 admin.site.register(Curs)
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    #list-view specific customizations
+    list_per_page = 10
+    search_fields = ('nume', 'prenume')
+    list_filter = ('an', 'cursuri')
+    list_display = ('nume', 'prenume', 'an')
+    #changeview customizations
+    fieldsets = [
+        ('', {
+            'fields': ['prenume', 'nume']
+         }
+        ),
+        ('Date Scolare', {
+            'fields': ['cursuri', 'an'],
+            'classes': ['collapse']
+        })
+    ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.none()
+        return qs
+    
+admin.site.register(Student, StudentAdmin)
