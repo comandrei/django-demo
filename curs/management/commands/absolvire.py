3a0e0b85e86af26585abde91a8f2235c54c423e7
Andrei Coman <comandrei@gmail.com>
2021-06-26 11:23:40 +0300

    Add arguments

--- curs/management/commands/absolvire.py
+++ curs/management/commands/absolvire.py
@@ -4,6 +4,10 @@ from django.db.models import F
 from curs.models import Student
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument('max_year', type=int)
+
+    def handle(self, *args, **options):
         print("Here")
+        print(options)
         Student.objects.update(an=F('an') + 1)
